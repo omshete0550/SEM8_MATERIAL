ba9958c4658cce3236022316214a883e0c1487f8
Om <omshete0550@gmail.com>
2025-04-09 09:44:14 +0530

    updates

--- DC/CODE/DC_EXP3/ClockSynchronization.java
+++ DC/CODE/DC_EXP3/ClockSynchronization.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
-class Process {
+class Process implements Runnable {
     private String name;
     private int clock;
-    private Scanner scanner;
+    private BlockingQueue<String> commandQueue = new LinkedBlockingQueue<>();
     private Process otherProcess;
-    private static final Object inputLock = new Object(); 
 
     public Process(String name, int initialTime) {
         this.name = name;
         this.clock = initialTime;
-        this.scanner = new Scanner(System.in);
     }
 
     public void setOtherProcess(Process otherProcess) {
@@ -18,7 +18,7 @@ class Process {
     }
 
     public void sendPacket() {
-        clock++; 
+        clock++;
         System.out.println(name + " sent a packet at time " + clock);
         otherProcess.receivePacket(clock);
     }
@@ -28,46 +28,62 @@ class Process {
         System.out.println(name + " received a packet at time " + clock);
     }
 
-    public void startProcess() {
+    public void enqueueCommand(String command) {
+        commandQueue.offer(command);
+    }
+
+    @Override
+    public void run() {
         System.out.println(name + " initial time: " + clock);
-
-        while (true) {
-            synchronized (inputLock) { 
-                System.out.print(name + ": Enter 'send' to send packet, 'exit' to quit: ");
-                String input = scanner.nextLine().trim().toLowerCase();
-
-                if (input.equals("exit")) {
+        try {
+            while (true) {
+                String command = commandQueue.take(); // blocks until command is received
+                if (command.equalsIgnoreCase("send")) {
+                    sendPacket();
+                } else if (command.equalsIgnoreCase("exit")) {
                     System.out.println(name + " is exiting...");
                     break;
-                } else if (input.equals("send")) {
-                    sendPacket();
-                } else {
-                    System.out.println("Invalid command. Please enter 'send' or 'exit'.");
                 }
             }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 }
 
 public class ClockSynchronization {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Process process1 = new Process("Process 1", 0);
         Process process2 = new Process("Process 2", 5);
 
         process1.setOtherProcess(process2);
         process2.setOtherProcess(process1);
 
-        Thread t1 = new Thread(process1::startProcess);
-        Thread t2 = new Thread(process2::startProcess);
+        Thread t1 = new Thread(process1);
+        Thread t2 = new Thread(process2);
 
-        t1.start(); 
+        t1.start();
+        t2.start();
 
-        try {
-            Thread.sleep(100); 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while (true) {
+            System.out.print("Enter command (e.g., '1 send', '2 send', '1 exit', '2 exit'): ");
+            String input = scanner.nextLine().trim().toLowerCase();
+
+            if (input.startsWith("1 ")) {
+                process1.enqueueCommand(input.substring(2));
+                if (input.endsWith("exit"))
+                    break;
+            } else if (input.startsWith("2 ")) {
+                process2.enqueueCommand(input.substring(2));
+                if (input.endsWith("exit"))
+                    break;
+            } else {
+                System.out.println("Invalid input. Use '1 send', '2 send', '1 exit', or '2 exit'.");
+            }
         }
 
-        t2.start(); 
+        scanner.close();
     }
 }
